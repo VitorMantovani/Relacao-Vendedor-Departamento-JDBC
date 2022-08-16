c08351c25485254546f7b23686e49812c071c44a
VitorMF11 <mantovanifreitasvitor@gmail.com>
2022-08-16 16:40:35 -0300

    DepartmentDao and SellerDao Interfaces

---
+++ src/br/com/fiap/dao/DepartmentDAO.java
@@ -0,0 +1,13 @@
+package br.com.fiap.dao;
+
+import br.com.fiap.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDAO {
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+}
---
+++ src/br/com/fiap/dao/SellerDao.java
@@ -0,0 +1,13 @@
+package br.com.fiap.dao;
+
+import br.com.fiap.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
